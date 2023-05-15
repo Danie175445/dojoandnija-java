d06b1673b5453980743efd8a317bd2f7d0b8eb6d
Daniel Castillo <danny4004@gmail.com>
2023-05-15 00:00:38 -0700

    first commit

---
+++ src/main/java/com/daniel/dojoandninja/model/Dojo.java
@@ -0,0 +1,75 @@
+package com.daniel.dojoandninja.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="dojo")
+public class Dojo {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="must be filled out")
+	private String name;
+	
+	@Column(updatable=false)
+	@DateTimeFormat(pattern="dd-mm-yyyy")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern="dd-mm-yyyy")
+	private Date updatedAt;
+	
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+	public Dojo (){
+		
+	}
+	
+	public Dojo(String name) {
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+}
